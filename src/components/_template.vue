c422e9a4ea1586aefcb3736c69fc4c2e9a9adf65
Xavier Drdak <x.drdak@gmail.com>
2017-12-03 14:43:55 -0500

    ghetto way to have a template file.

---
+++ src/components/_template.vue
@@ -0,0 +1,45 @@
+<template>
+  <ComponentEditor>
+    <h1 slot="component-name">__INSERT COMPONENT NAME__</h1>
+    <div slot="editor-pane">
+    </div>
+    <div slot="component-preview" v-html="computedSafeComponent"></div>
+    <code class="html" slot="component-raw">{{ computedComponent }}</code>
+  </ComponentEditor>
+</template>
+
+<script>
+// import sanitize from '@/utils/sanitize';
+// import converter from '@/tachyons-utils/converter';
+import ComponentEditor from '@/slot-templates/ComponentEditor';
+import TextInput from '@/components/form-elements/TextInput';
+import DropdownList from '@/components/form-elements/DropdownList';
+import DropdownListColors from '@/components/form-elements/DropdownListColors';
+
+const component = () => ''.trim();
+
+export default {
+  name: 'input-with-addon',
+  components: {
+    DropdownList,
+    DropdownListColors,
+    TextInput,
+    ComponentEditor,
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+    computedClass() {
+      return '';
+    },
+    computedComponent() {
+      return component();
+    },
+    computedSafeComponent() {
+      return component();
+    },
+  },
+};
+</script>
